25db961889bb5bbd40a52ea3b83a184b5658e576
hmrob <77305301+hmrob@users.noreply.github.com>
2024-03-15 13:07:47 -0700

    fixing list problem

--- listOps.js
+++ listOps.js
@@ -7,21 +7,26 @@ async function viewList() {
   //list = '<div id="list-content"><div name="check" id="1_div"><input type="checkbox" id="1" onclick="checkOff(\'1_div\')"><label for="1">1 14 oz can crushed pineapple, drained</label></div><div name="check" id="2_div"><input type="checkbox" id="2" onclick="checkOff(\'2_div\')"><label for="2">1 14 oz can fruit cocktail, drained</label></div><div name="check" id="3_div"><input type="checkbox" id="3" onclick="checkOff(\'3_div\')"><label for="3">1 pouch pistachio pudding mix</label></div><div name="check" id="4_div"><input type="checkbox" id="4" onclick="checkOff(\'4_div\')"><label for="4">1 8 oz container cool whip</label></div><div name="check" id="5_div"><input type="checkbox" id="5" onclick="checkOff(\'5_div\')"><label for="5">2 c mini marshmallows</label></div><div name="check" id="6_div"><input type="checkbox" id="6" onclick="checkOff(\'6_div\')"><label for="6">1 c chopped nuts (opt)</label></div></div>';
 
   var listDiv = document.getElementById("list-content");
+
+  var list = "";
+
+  var ingredients = localStorage.getItem("list").split("\n")
+  for (i = 0; i < ingredients.length; i++) {
+    if (ingredients[i].length > 2) {
+      var divID = i + "_div";
+      list = list + '\n' + chkTemplate.replaceAll('[chk_id]', i + "").replaceAll('[chk_txt]', ingredients[i]).replaceAll('[div_id]', divID);
+    }
+  }
+
   listDiv.innerHTML = list;
   await openModal('listModal');
 }
 
 function addToGroceryList() {
   var ingredients = document.getElementById("ingredientsText").innerText.split('\n');
-  console.log(ingredients)
   itemExists("list")
   var currList = localStorage.getItem("list")
-  for (i = 0; i < ingredients.length; i++) {
-    if (ingredients[i].length > 2) {
-      var divID = i + "_div";
-      currList = currList + '\n' + chkTemplate.replaceAll('[chk_id]', i + "").replaceAll('[chk_txt]', ingredients[i]).replaceAll('[div_id]', divID);
-    }
-  }
+  currList = currList + ingredients;
   localStorage.setItem("list", currList)
 }
 
